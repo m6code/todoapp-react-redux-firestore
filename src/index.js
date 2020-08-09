31206ae5e935c8a22801c50e04b3a465cee1756a
M6code <benjamynfrank101@gmail.com>
2020-08-09 06:06:11 +0100

    create store, set up react-redux-firestore config

--- src/index.js
+++ src/index.js
@@ -8,19 +8,44 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import firebaseConfig from './config/firebaseConfig'
+// React, Redux, firebase
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
+import { createFirestoreInstance } from 'redux-firestore'
+import { rootReducer } from './ducks/reducers'
+// React Router
+import { BrowserRouter } from 'react-router-dom'
 
-
-firebase.initializeApp(firebaseConfig)
-firebase.firestore();
 
 const rrfConfig = {
   userProfile: "users",
   useFirestoreForProfile: true,
 }
 
+firebase.initializeApp(firebaseConfig)
+firebase.firestore();
+
+const initState = {};
+const store = createStore(rootReducer, initState);
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance, // since we are using firestore
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
